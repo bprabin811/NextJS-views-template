bd8aaaf75c5a18e43b0533c80c3528a8eb6e2340
Prabin Bhatt <prabin.bhatt@braintip.ai>
2024-11-18 21:24:10 +0545

    initial commit

---
+++ src/app/examples/calendar/page.tsx
@@ -0,0 +1,11 @@
+import { Calendar } from "lucide-react";
+
+const CalendarPage = () => {
+  return (
+    <div className="flex items-center space-x-2">
+      <Calendar className="h-4 w-4" />
+    </div>
+  );
+};
+
+export default CalendarPage;
